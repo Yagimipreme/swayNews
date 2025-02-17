dec0fd8cd833ac141100f87d7374d37405a40379
Yagimipreme <asbalk@gmx.de>
2025-02-17 12:49:54 +0000

    taz_tryingOut

--- taz.py
+++ taz.py
@@ -1,15 +1,25 @@
 import re # regex um links zu filtern
 import requests
 from bs4 import BeautifulSoup
-url = "https://www.welt.de"
+url = "https://www.taz.de"
 r = requests.get(url)
 s = BeautifulSoup(r.text, "html.parser")
 
 #Section = abschnitt ie. Bild d. Tages, Themen d. Tages...
 def grepArticles(section):
-    articles=section.find_all(lambda tag: tag.name == "article" and tag.get("data-external") == "Teaser")
-    has_article = False
-    for article in articles:
+    links = section.find_all("a", href=True)
+    for link in links:
+        print(link)
+        print("LINK :"+link.text.strip())
+        paragraphs = link.find_all("p")
+        for paragraph in paragraphs :
+            if paragraph :
+                print("PARAG :"+paragraph.text)
+                print("------------------")
+            else :
+                continue
+
+        '''
         link = article.find("a", href=True)
         if link and "Ressort" not in article.text:
             if not has_article :
@@ -18,10 +28,14 @@ def grepArticles(section):
             else : 
                 print("----------------------------")
                 print(link.text)
+                '''
 
 for section in s.find_all("section"):
+    grepArticles(section)
+    '''
     if "Anzeige" in section.text.strip(): 
         continue
     else :
         #print("SECTION :"+section.text)
         grepArticles(section)
+        '''
